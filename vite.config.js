53f440ca14decdd3ab45ff340e31d483607996b7
aricgui <aricgui258gmail.com>
2026-04-23 13:29:23 +0200

    décimocuarto commit

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/practica4/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/padelfip': {
-        target: 'https://www.padelfip.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/padelfip/, ''),
-      },
-      '/wikimedia': {
-        target: 'https://upload.wikimedia.org',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/wikimedia/, ''),
-      },
-    },
-  },
-  preview: {
-    proxy: {
-      '/padelfip': {
-        target: 'https://www.padelfip.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/padelfip/, ''),
-      },
-      '/wikimedia': {
-        target: 'https://upload.wikimedia.org',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/wikimedia/, ''),
-      },
-    },
-  },
+  base: '/practica4/',
 })
